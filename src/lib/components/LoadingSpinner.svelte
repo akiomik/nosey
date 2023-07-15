f68b6f817b955cff7c555a56ffa90bfa57609a68
Akiomi Kamakura <akiomik@gmail.com>
2023-07-16 08:11:33 +0900

    Improve navigation

---
+++ src/lib/components/LoadingSpinner.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  type Size = 'sm' | 'md';
+  export let size: Size = 'md';
+</script>
+
+<div class="grid place-items-center" class:h-96={size === 'md'} class:h-48={size === 'sm'}>
+  <div
+    class="animate-spin h-12 w-12 border-4 border-primary-500 rounded-full border-t-transparent"
+  />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,24 @@
   import '../app.postcss';
 
   import { AppShell } from '@skeletonlabs/skeleton';
+  import { navigating } from '$app/stores';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
 </script>
 
 <AppShell>
   <svelte:fragment slot="header">
     <Header />
   </svelte:fragment>
+
+  {#if $navigating}
+    <div class="fixed inset-0 z-[15] bg-black opacity-60">
+      <div class="flex justify-center items-center w-full h-full">
+        <LoadingSpinner />
+      </div>
+    </div>
+  {/if}
 
   <div class="container mx-auto max-w-4xl p-4 space-y-8 mt-4 h-full">
     <slot />
