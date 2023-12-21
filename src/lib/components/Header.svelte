673ab396fcdb8377a2f7fd37ea4065363ee2979d
Akiomi Kamakura <akiomik@gmail.com>
2023-12-21 13:47:22 +0900

    Add HeaderMenu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
   import { AppBar } from '@skeletonlabs/skeleton';
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+  import { faBars } from '@fortawesome/free-solid-svg-icons';
+  import { popup } from '@skeletonlabs/skeleton';
+  import type { PopupSettings } from '@skeletonlabs/skeleton';
+  import HeaderMenu from './HeaderMenu.svelte';
+
+  const popupTargetId = 'header-menu';
+  const menuPopup: PopupSettings = {
+    event: 'click',
+    target: popupTargetId,
+    placement: 'bottom',
+  };
 </script>
 
-<AppBar>
+<AppBar slotTrail="place-content-end">
   <a href="/">nosey</a>
+  <svelte:fragment slot="trail">
+    <button class="btn-icon" use:popup={menuPopup}>
+      <FontAwesomeIcon icon={faBars} title="Open menu" class="w-4" />
+    </button>
+  </svelte:fragment>
 </AppBar>
+
+<HeaderMenu popupId={popupTargetId} />
---
+++ src/lib/components/HeaderMenu.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let popupId: string;
+</script>
+
+<div class="card p-2 w-52 z-10 shadow" data-popup={popupId}>
+  <ul class="list-nav">
+    <li>
+      <button class="w-full">Advanced search </button>
+    </li>
+  </ul>
+</div>
